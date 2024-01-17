9bae2c0ad4eee1253e04e8ad18ebd8fe765973e5
OrlyIdeate <ueno.jin25@outlook.jp>
2024-01-17 23:20:04 +0900

    add: クエリ実行関数追加

--- database/query.php
+++ database/query.php
@@ -1,8 +1,11 @@
 <?php
+// 自動でライブラリ読み込み
+require '../vendor/autoload.php';
+
 // PDOを使って、SQLのクエリーを引数に渡すだけで、クエリーを実行してくれる関数を作成
 function executeQuery($query) {
     // .envを使用する
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
     $dotenv->load();
 
     $host = $_ENV["HOST"];
@@ -16,6 +19,10 @@ function executeQuery($query) {
         // クエリを実行
         $stmt = $pdo->prepare($query);
         $stmt->execute();
+
+        if (stripos($query, 'SELECT') === 0) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
     } catch (PDOException $e) {
         echo 'Connection failed: ' . $e->getMessage();
     }
